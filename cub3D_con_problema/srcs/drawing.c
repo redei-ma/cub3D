63cba819d6ef0e33712c24f908c1cddb52383d2a
cacatuacommander <144157854+cacatuacommander@users.noreply.github.com>
2025-07-07 18:15:28 +0200

    Add files via upload

---
+++ cub3D_con_problema/srcs/drawing.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   drawing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-ross <ade-ross@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 10:13:26 by redei-ma          #+#    #+#             */
+/*   Updated: 2025/07/07 15:21:09 by ade-ross         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3D.h"
+
+void	put_pixel_to_image(t_image img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x >= 0 && x < WIN_WIDTH && y >= 0 && y < WIN_HEIGHT)
+	{
+		dst = img.addr + (y * img.line_length + x * (img.bits_per_pixel / 8));
+		*(unsigned int *)dst = color;
+	}
+}
+
+static void	draw_single_ray(t_data *data, float angle, t_minimap mini)
+{
+	float	wall_dist;
+	float	current_x;
+	float	current_y;
+	float	step_size;
+	int		i;
+
+	wall_dist = cast_ray_dda(data, data->player->x, data->player->y, angle);
+	step_size = 0.02f;
+	current_x = data->player->x;
+	current_y = data->player->y;
+	i = 0;
+	while (i < (int)(wall_dist / step_size))
+	{
+		put_pixel_to_image(data->game->img,
+			(int)(current_x * mini.tile_size_x),
+			(int)(current_y * mini.tile_size_y),
+			0xFF0000);
+		current_x += cos(angle) * step_size;
+		current_y += sin(angle) * step_size;
+		i++;
+	}
+}
+
+static void	draw_fov_to_image(t_data *data)
+{
+	float			start_angle;
+	float			angle_step;
+	int				i;
+
+	angle_step = FOV_ANGLE / FOV_RAYS;
+	start_angle = data->player->angle - FOV_ANGLE / 2;
+	i = 0;
+	while (i < FOV_RAYS)
+	{
+		draw_single_ray(data, start_angle + i * angle_step, data->mini);
+		i++;
+	}
+}
+
+static void	draw_player_to_image(t_data *data, int center_x,
+		int center_y, int color)
+{
+	int				radius;
+	int				i;
+	int				j;
+
+	radius = data->mini.tile_size_x / PLAYER_SIZE_RATIO;
+	j = -radius;
+	while (j <= radius)
+	{
+		i = -radius;
+		while (i <= radius)
+		{
+			if (i * i + j * j <= radius * radius)
+				put_pixel_to_image(data->game->img, center_x + i,
+					center_y + j, color);
+			i++;
+		}
+		j++;
+	}
+}
+
+void	draw_image(t_data *data)
+{
+	draw_3d_to_image(data);
+	draw_minimap_to_image(data);
+	draw_player_to_image(data, data->player->x * data->mini.tile_size_x,
+		data->player->y * data->mini.tile_size_y, 0x00FF00);
+	draw_fov_to_image(data);
+	mlx_put_image_to_window(data->game->mlx, data->game->win,
+		data->game->img.img, 0, 0);
+}
---
+++ cub3D_con_problema/srcs/init.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-ross <ade-ross@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 10:15:44 by redei-ma          #+#    #+#             */
+/*   Updated: 2025/07/07 15:01:15 by ade-ross         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3D.h"
+
+t_minimap	init_minimap(char **map)
+{
+	t_minimap	mini;
+
+	mini.map_width = ft_strlen(map[0]);
+	mini.map_height = ft_countlines((const char **)map);
+	mini.minimap_width = WIN_WIDTH * 0.2;
+	mini.minimap_height = WIN_HEIGHT * 0.2;
+	mini.tile_size_x = mini.minimap_width / mini.map_width;
+	mini.tile_size_y = mini.minimap_height / mini.map_height;
+	return (mini);
+}
+
+static int	init_image(t_game *game)
+{
+	game->img.img = mlx_new_image(game->mlx, WIN_WIDTH, WIN_HEIGHT);
+	if (!game->img.img)
+		return (0);
+	game->img.addr = mlx_get_data_addr(game->img.img, &game->img.bits_per_pixel,
+			&game->img.line_length, &game->img.endian);
+	return (1);
+}
+
+t_game	*init_game(void)
+{
+	t_game	*game;
+
+	game = ft_calloc(1, sizeof(t_game));
+	if (!game)
+		return (NULL);
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		return (free(game), NULL);
+	game->win = mlx_new_window(game->mlx, WIN_WIDTH, WIN_HEIGHT, "CUB3D");
+	if (!game->win)
+	{
+		free(game->mlx);
+		free(game);
+		return (NULL);
+	}
+	if (!init_image(game))
+	{
+		mlx_destroy_window(game->mlx, game->win);
+		free(game->mlx);
+		free(game);
+		return (NULL);
+	}
+	ft_memset(game->keys_pressed, 0, sizeof(game->keys_pressed));
+	game->animation_frame = 0;
+	return (game);
+}
